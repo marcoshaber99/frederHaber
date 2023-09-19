82d49d0159790a844dfa2d2c9fe93bc2ae1a6f9e
Marco Haber <marcoshaber99@gmail.com>
2023-09-19 14:25:49 +0300

    finalizing

--- server/routes/scholarship.routes.js
+++ server/routes/scholarship.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer(); // setup multer
+const upload = multer(); 
 
 const { 
     createRequest,
